9c60255998388483200843f41650331257014ded
bahadır çolak <bahdrcolak@outlook.com.tr>
2017-05-19 16:13:20 +0300

    Login Screen

--- Bil372Proje/Bil372Proje/Login.Designer.cs
+++ Bil372Proje/Bil372Proje/Login.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.Label();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.register_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(354, 172);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(354, 238);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(163, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Location = new System.Drawing.Point(185, 238);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(69, 17);
+            this.password.TabIndex = 2;
+            this.password.Text = "Password";
+            // 
+            // username
+            // 
+            this.username.AutoSize = true;
+            this.username.Location = new System.Drawing.Point(185, 172);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(73, 17);
+            this.username.TabIndex = 3;
+            this.username.Text = "Username";
+            this.username.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // login_btn
+            // 
+            this.login_btn.Location = new System.Drawing.Point(354, 300);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(75, 23);
+            this.login_btn.TabIndex = 4;
+            this.login_btn.Text = "Login";
+            this.login_btn.UseVisualStyleBackColor = true;
+            // 
+            // register_btn
+            // 
+            this.register_btn.Location = new System.Drawing.Point(442, 300);
+            this.register_btn.Name = "register_btn";
+            this.register_btn.Size = new System.Drawing.Size(75, 23);
+            this.register_btn.TabIndex = 5;
+            this.register_btn.Text = "Register";
+            this.register_btn.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 476);
+            this.Controls.Add(this.register_btn);
+            this.Controls.Add(this.login_btn);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Login";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.Label username;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Button register_btn;
     }
 }
 
--- Bil372Proje/Bil372Proje/Login.cs
+++ Bil372Proje/Bil372Proje/Login.cs
@@ -16,5 +16,10 @@ namespace Bil372Proje
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
