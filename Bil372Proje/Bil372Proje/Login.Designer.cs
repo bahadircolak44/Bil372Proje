33960d39db068206d8eaedc759866e49a6fec62b
bahadır çolak <bahdrcolak@outlook.com.tr>
2017-05-22 01:26:32 +0300

    Login System Done

--- Bil372Proje/Bil372Proje/Login.Designer.cs
+++ Bil372Proje/Bil372Proje/Login.Designer.cs
@@ -3,14 +3,14 @@
     partial class Login
     {
         /// <summary>
-        ///Gerekli tasarımcı değişkeni.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///Kullanılan tüm kaynakları temizleyin.
+        /// Clean up any resources being used.
         /// </summary>
-        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,86 +20,86 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer üretilen kod
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Tasarımcı desteği için gerekli metot - bu metodun 
-        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.password = new System.Windows.Forms.Label();
-            this.username = new System.Windows.Forms.Label();
-            this.login_btn = new System.Windows.Forms.Button();
-            this.register_btn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password : ";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(354, 172);
+            this.textBox1.Location = new System.Drawing.Point(154, 43);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(163, 22);
-            this.textBox1.TabIndex = 0;
+            this.textBox1.Size = new System.Drawing.Size(201, 22);
+            this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(354, 238);
+            this.textBox2.Location = new System.Drawing.Point(154, 106);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(163, 22);
-            this.textBox2.TabIndex = 1;
+            this.textBox2.Size = new System.Drawing.Size(201, 22);
+            this.textBox2.TabIndex = 3;
             // 
-            // password
+            // button1
             // 
-            this.password.AutoSize = true;
-            this.password.Location = new System.Drawing.Point(185, 238);
-            this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(69, 17);
-            this.password.TabIndex = 2;
-            this.password.Text = "Password";
+            this.button1.Location = new System.Drawing.Point(154, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // username
+            // button2
             // 
-            this.username.AutoSize = true;
-            this.username.Location = new System.Drawing.Point(185, 172);
-            this.username.Name = "username";
-            this.username.Size = new System.Drawing.Size(73, 17);
-            this.username.TabIndex = 3;
-            this.username.Text = "Username";
-            this.username.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // login_btn
-            // 
-            this.login_btn.Location = new System.Drawing.Point(354, 300);
-            this.login_btn.Name = "login_btn";
-            this.login_btn.Size = new System.Drawing.Size(75, 23);
-            this.login_btn.TabIndex = 4;
-            this.login_btn.Text = "Login";
-            this.login_btn.UseVisualStyleBackColor = true;
-            // 
-            // register_btn
-            // 
-            this.register_btn.Location = new System.Drawing.Point(442, 300);
-            this.register_btn.Name = "register_btn";
-            this.register_btn.Size = new System.Drawing.Size(75, 23);
-            this.register_btn.TabIndex = 5;
-            this.register_btn.Text = "Register";
-            this.register_btn.UseVisualStyleBackColor = true;
+            this.button2.Location = new System.Drawing.Point(280, 170);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "register";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(872, 476);
-            this.Controls.Add(this.register_btn);
-            this.Controls.Add(this.login_btn);
-            this.Controls.Add(this.username);
-            this.Controls.Add(this.password);
+            this.ClientSize = new System.Drawing.Size(421, 217);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Login";
-            this.Text = "Form1";
+            this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,12 +107,11 @@
 
         #endregion
 
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label password;
-        private System.Windows.Forms.Label username;
-        private System.Windows.Forms.Button login_btn;
-        private System.Windows.Forms.Button register_btn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
-
--- Bil372Proje/Bil372Proje/Login.cs
+++ Bil372Proje/Bil372Proje/Login.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Bil372Proje
 {
@@ -17,9 +12,20 @@ namespace Bil372Proje
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-
+            SqlConnection con = new SqlConnection("Data Source=bil372.database.windows.net;Initial Catalog=bil372DB;User ID=bahadir;Password=Qwerty123");
+            SqlDataAdapter sda = new SqlDataAdapter("Select Id From users where username='"+ textBox1.Text+"'and password='"+ textBox2.Text+"'",con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                MessageBox.Show("Doğru");
+            }
+            else
+            {
+                MessageBox.Show("Yanlış");
+            }
         }
     }
 }
