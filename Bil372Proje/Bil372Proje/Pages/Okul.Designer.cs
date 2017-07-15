0d1628f38f0406c152608fabf0c75ec313221216
bahadır çolak <bahdrcolak@outlook.com.tr>
2017-07-15 03:29:26 +0300

    sistem devam

--- Bil372Proje/Bil372Proje/Pages/Okul.Designer.cs
+++ Bil372Proje/Bil372Proje/Pages/Okul.Designer.cs
@@ -28,19 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1134, 544);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Okul
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(822, 554);
+            this.ClientSize = new System.Drawing.Size(1158, 668);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Okul";
             this.Text = "Okul";
+            this.Load += new System.EventHandler(this.Okul_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Bil372Proje/Bil372Proje/Pages/Okul.cs
+++ Bil372Proje/Bil372Proje/Pages/Okul.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,37 @@ namespace Bil372Proje.Pages
         public Okul()
         {
             InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=bil372.database.windows.net;Initial Catalog=bil372DB;User ID=bahadir;Password=Qwerty123");
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void kayitGetir()
+        {
+            baglanti.Open();
+            string kayit = "SELECT k.isim,k.adet,g.isim,g.adet,ki.isim,ki.adet from okul o " +
+                "inner join kirtasiye k on o.Id=k.okul_id " +
+                "inner join giysi g on o.Id=g.okul_id " +
+                "inner join kitap ki on o.Id=ki.okul_id " ;
+            //musteriler tablosundaki tüm kayıtları çekecek olan sql sorgusu.
+            SqlCommand komut = new SqlCommand(kayit, baglanti);
+            //Sorgumuzu ve baglantimizi parametre olarak alan bir SqlCommand nesnesi oluşturuyoruz.
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            //SqlDataAdapter sınıfı verilerin databaseden aktarılması işlemini gerçekleştirir.
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            //Bir DataTable oluşturarak DataAdapter ile getirilen verileri tablo içerisine dolduruyoruz.
+            dataGridView1.DataSource = dt;
+            //Formumuzdaki DataGridViewin veri kaynağını oluşturduğumuz tablo olarak gösteriyoruz.
+            baglanti.Close();
+        }
+
+        private void Okul_Load(object sender, EventArgs e)
+        {
+            kayitGetir();
         }
     }
 }
