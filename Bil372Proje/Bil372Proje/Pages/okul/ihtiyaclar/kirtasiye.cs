13b933f59d071a27d9b73151ec971456c84ef895
bahadır çolak <bahdrcolak@outlook.com.tr>
2017-07-17 20:48:46 +0300

    adet güncelleme sistemi hazır 17.07

--- Bil372Proje/Bil372Proje/Pages/okul/ihtiyaclar/kirtasiye.cs
+++ Bil372Proje/Bil372Proje/Pages/okul/ihtiyaclar/kirtasiye.cs
@@ -36,16 +36,40 @@ namespace Bil372Proje.Pages.okul.ihtiyaclar
         private void button1_Click(object sender, EventArgs e)
         {
             con.Open();
+            SqlCommand kontrol = new SqlCommand("select adet from ihtiyac where isim=@isim", con);
+            kontrol.Parameters.AddWithValue("@isim", kirtasiye_ihtiyac.Text);
+            SqlDataAdapter adapt = new SqlDataAdapter(kontrol);
+            DataSet ds = new DataSet();
+            adapt.Fill(ds);
+            int i = ds.Tables[0].Rows.Count;
+            if (i > 0)
+            {
+                SqlCommand cmd = new SqlCommand("Update ihtiyac set adet=@Adet Where isim = @isim", con);
+                int cnt = Convert.ToInt32(kontrol.ExecuteScalar())+Convert.ToInt32(kirtasiye_adet.Text.Trim());
+                MessageBox.Show(cnt.ToString());
+                cmd.Parameters.AddWithValue("@Adet", cnt);
+                cmd.Parameters.AddWithValue("@isim", kirtasiye_ihtiyac.Text);
+                cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand("insert into ihtiyac(okul_id,isim,adet,marka)" +
+    " values(@okul_id,@isim,@adet,@marka)", con);
+                cmd.Parameters.AddWithValue("@isim", kirtasiye_ihtiyac.Text);
+                cmd.Parameters.AddWithValue("@adet", kirtasiye_adet.Text);
+                cmd.Parameters.AddWithValue("@marka", kirtasiye_marka.Text);
+                cmd.Parameters.AddWithValue("@okul_id", okul_id);
+                cmd.ExecuteNonQuery();
+            }
 
-            SqlCommand komut2 = new SqlCommand("select adet from kirtasiye where isim=@isim", con);
 
-            SqlCommand cmd = new SqlCommand("insert into kirtasiye(Id,okul_id,isim,adet,marka)" +
-                " values(@Id,@okul_id,@isim,@adet,@marka)", con);
-            cmd.Parameters.AddWithValue("@isim",kirtasiye_ihtiyac.Text);
-            cmd.Parameters.AddWithValue("@adet", kirtasiye_adet.Text);
-            cmd.Parameters.AddWithValue("@marka", kirtasiye_marka.Text);
-            cmd.Parameters.AddWithValue("@okul_id", okul_id);
-            cmd.ExecuteNonQuery();
+
+            SqlCommand komut = new SqlCommand("select max(id) from ihtiyac", con);
+            int count = Convert.ToInt32(komut.ExecuteScalar());
+            SqlCommand cmd2 = new SqlCommand("insert into kirtasiye(ihtiyac_id)" +
+            " values(@ihtiyac_id)", con);
+            cmd2.Parameters.AddWithValue("@ihtiyac_id", count);
+            cmd2.ExecuteNonQuery();
             kayitGetir();
             clear();
         }
@@ -58,8 +82,8 @@ namespace Bil372Proje.Pages.okul.ihtiyaclar
         }
         private void kayitGetir()
         {
-            string kayit = "SELECT k.isim,k.adet,k.marka" +
-                " from kirtasiye k where k.okul_id =@okul_id";
+            string kayit = "SELECT isim,adet,marka" +
+                " from ihtiyac where ihtiyac.okul_id =@okul_id";
 
             //musteriler tablosundaki tüm kayıtları çekecek olan sql sorgusu.
             SqlCommand komut = new SqlCommand(kayit, con);
