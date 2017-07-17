2fd7a8c223a809a8fc3f4794fd5cbdc93a6aa9d3
Daghan SENGOREN <daghan.sengoren@gmail.com>
2017-07-18 00:34:16 +0300

    teknoloji ve kitap eklendi

--- Bil372Proje/Bil372Proje/Pages/okul/ihtiyaclar/giysi.Designer.cs
+++ Bil372Proje/Bil372Proje/Pages/okul/ihtiyaclar/giysi.Designer.cs
@@ -29,19 +29,19 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.giysi_ihtiyac = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.giysi_cinsiyet = new System.Windows.Forms.Label();
-            this.giysi_beden = new System.Windows.Forms.TextBox();
-            this.giysi_renk = new System.Windows.Forms.TextBox();
-            this.giysi_kumas = new System.Windows.Forms.TextBox();
             this.giysi_checkbox_erkek = new System.Windows.Forms.CheckBox();
             this.giysi_checkbox_kadin = new System.Windows.Forms.CheckBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.giysi_ihtiyac = new System.Windows.Forms.ComboBox();
+            this.giysi_beden = new System.Windows.Forms.ComboBox();
+            this.giysi_renk = new System.Windows.Forms.ComboBox();
+            this.giysi_kumas = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -49,29 +49,19 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(203, 55);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(152, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "ihtiyaç : ";
-            // 
-            // giysi_ihtiyac
-            // 
-            this.giysi_ihtiyac.Location = new System.Drawing.Point(347, 55);
-            this.giysi_ihtiyac.Margin = new System.Windows.Forms.Padding(4);
-            this.giysi_ihtiyac.Name = "giysi_ihtiyac";
-            this.giysi_ihtiyac.Size = new System.Drawing.Size(332, 22);
-            this.giysi_ihtiyac.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(203, 138);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(152, 112);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 2;
             this.label2.Text = "Beden :";
             // 
@@ -79,10 +69,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(201, 222);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(151, 180);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 17);
+            this.label3.Size = new System.Drawing.Size(45, 13);
             this.label3.TabIndex = 3;
             this.label3.Text = "Renk :";
             // 
@@ -90,10 +79,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(203, 303);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(152, 246);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.Size = new System.Drawing.Size(52, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "Kumas :";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -102,45 +90,19 @@
             // 
             this.giysi_cinsiyet.AutoSize = true;
             this.giysi_cinsiyet.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.giysi_cinsiyet.Location = new System.Drawing.Point(203, 369);
-            this.giysi_cinsiyet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.giysi_cinsiyet.Location = new System.Drawing.Point(152, 300);
             this.giysi_cinsiyet.Name = "giysi_cinsiyet";
-            this.giysi_cinsiyet.Size = new System.Drawing.Size(75, 17);
+            this.giysi_cinsiyet.Size = new System.Drawing.Size(59, 13);
             this.giysi_cinsiyet.TabIndex = 5;
             this.giysi_cinsiyet.Text = "Cinsiyet :";
-            // 
-            // giysi_beden
-            // 
-            this.giysi_beden.Location = new System.Drawing.Point(347, 138);
-            this.giysi_beden.Margin = new System.Windows.Forms.Padding(4);
-            this.giysi_beden.Name = "giysi_beden";
-            this.giysi_beden.Size = new System.Drawing.Size(332, 22);
-            this.giysi_beden.TabIndex = 6;
-            // 
-            // giysi_renk
-            // 
-            this.giysi_renk.Location = new System.Drawing.Point(347, 222);
-            this.giysi_renk.Margin = new System.Windows.Forms.Padding(4);
-            this.giysi_renk.Name = "giysi_renk";
-            this.giysi_renk.Size = new System.Drawing.Size(332, 22);
-            this.giysi_renk.TabIndex = 7;
-            // 
-            // giysi_kumas
-            // 
-            this.giysi_kumas.Location = new System.Drawing.Point(347, 303);
-            this.giysi_kumas.Margin = new System.Windows.Forms.Padding(4);
-            this.giysi_kumas.Name = "giysi_kumas";
-            this.giysi_kumas.Size = new System.Drawing.Size(332, 22);
-            this.giysi_kumas.TabIndex = 8;
             // 
             // giysi_checkbox_erkek
             // 
             this.giysi_checkbox_erkek.AutoSize = true;
             this.giysi_checkbox_erkek.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.giysi_checkbox_erkek.Location = new System.Drawing.Point(368, 364);
-            this.giysi_checkbox_erkek.Margin = new System.Windows.Forms.Padding(4);
+            this.giysi_checkbox_erkek.Location = new System.Drawing.Point(276, 296);
             this.giysi_checkbox_erkek.Name = "giysi_checkbox_erkek";
-            this.giysi_checkbox_erkek.Size = new System.Drawing.Size(71, 21);
+            this.giysi_checkbox_erkek.Size = new System.Drawing.Size(59, 17);
             this.giysi_checkbox_erkek.TabIndex = 9;
             this.giysi_checkbox_erkek.Text = "Erkek";
             this.giysi_checkbox_erkek.UseVisualStyleBackColor = true;
@@ -149,10 +111,9 @@
             // 
             this.giysi_checkbox_kadin.AutoSize = true;
             this.giysi_checkbox_kadin.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.giysi_checkbox_kadin.Location = new System.Drawing.Point(559, 364);
-            this.giysi_checkbox_kadin.Margin = new System.Windows.Forms.Padding(4);
+            this.giysi_checkbox_kadin.Location = new System.Drawing.Point(419, 296);
             this.giysi_checkbox_kadin.Name = "giysi_checkbox_kadin";
-            this.giysi_checkbox_kadin.Size = new System.Drawing.Size(71, 21);
+            this.giysi_checkbox_kadin.Size = new System.Drawing.Size(58, 17);
             this.giysi_checkbox_kadin.TabIndex = 10;
             this.giysi_checkbox_kadin.Text = "Kadin";
             this.giysi_checkbox_kadin.UseVisualStyleBackColor = true;
@@ -160,10 +121,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.button1.Location = new System.Drawing.Point(347, 423);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(260, 344);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 41);
+            this.button1.Size = new System.Drawing.Size(94, 33);
             this.button1.TabIndex = 11;
             this.button1.Text = "Ekle";
             this.button1.UseVisualStyleBackColor = true;
@@ -171,10 +131,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.button2.Location = new System.Drawing.Point(559, 423);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(419, 344);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(129, 39);
+            this.button2.Size = new System.Drawing.Size(97, 32);
             this.button2.TabIndex = 12;
             this.button2.Text = "Geri";
             this.button2.UseVisualStyleBackColor = true;
@@ -182,33 +141,66 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(786, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(590, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(361, 452);
+            this.dataGridView1.Size = new System.Drawing.Size(271, 367);
             this.dataGridView1.TabIndex = 13;
+            // 
+            // giysi_ihtiyac
+            // 
+            this.giysi_ihtiyac.FormattingEnabled = true;
+            this.giysi_ihtiyac.Location = new System.Drawing.Point(276, 45);
+            this.giysi_ihtiyac.Name = "giysi_ihtiyac";
+            this.giysi_ihtiyac.Size = new System.Drawing.Size(207, 21);
+            this.giysi_ihtiyac.TabIndex = 14;
+            // 
+            // giysi_beden
+            // 
+            this.giysi_beden.FormattingEnabled = true;
+            this.giysi_beden.Location = new System.Drawing.Point(276, 112);
+            this.giysi_beden.Name = "giysi_beden";
+            this.giysi_beden.Size = new System.Drawing.Size(207, 21);
+            this.giysi_beden.TabIndex = 15;
+            // 
+            // giysi_renk
+            // 
+            this.giysi_renk.FormattingEnabled = true;
+            this.giysi_renk.Location = new System.Drawing.Point(276, 180);
+            this.giysi_renk.Name = "giysi_renk";
+            this.giysi_renk.Size = new System.Drawing.Size(207, 21);
+            this.giysi_renk.TabIndex = 16;
+            // 
+            // giysi_kumas
+            // 
+            this.giysi_kumas.FormattingEnabled = true;
+            this.giysi_kumas.Location = new System.Drawing.Point(276, 246);
+            this.giysi_kumas.Name = "giysi_kumas";
+            this.giysi_kumas.Size = new System.Drawing.Size(207, 21);
+            this.giysi_kumas.TabIndex = 17;
             // 
             // giysi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(1159, 692);
+            this.ClientSize = new System.Drawing.Size(869, 562);
+            this.Controls.Add(this.giysi_kumas);
+            this.Controls.Add(this.giysi_renk);
+            this.Controls.Add(this.giysi_beden);
+            this.Controls.Add(this.giysi_ihtiyac);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.giysi_checkbox_kadin);
             this.Controls.Add(this.giysi_checkbox_erkek);
-            this.Controls.Add(this.giysi_kumas);
-            this.Controls.Add(this.giysi_renk);
-            this.Controls.Add(this.giysi_beden);
             this.Controls.Add(this.giysi_cinsiyet);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.giysi_ihtiyac);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "giysi";
             this.Text = "giysi";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -220,18 +212,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox giysi_ihtiyac;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label giysi_cinsiyet;
-        private System.Windows.Forms.TextBox giysi_beden;
-        private System.Windows.Forms.TextBox giysi_renk;
-        private System.Windows.Forms.TextBox giysi_kumas;
         private System.Windows.Forms.CheckBox giysi_checkbox_erkek;
         private System.Windows.Forms.CheckBox giysi_checkbox_kadin;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox giysi_ihtiyac;
+        private System.Windows.Forms.ComboBox giysi_beden;
+        private System.Windows.Forms.ComboBox giysi_renk;
+        private System.Windows.Forms.ComboBox giysi_kumas;
     }
 }
