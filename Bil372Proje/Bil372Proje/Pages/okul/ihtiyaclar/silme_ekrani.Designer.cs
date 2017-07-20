9666f8cfd86bef561336c6ae8a4049df3698dbc4
bahadır çolak <bahdrcolak@outlook.com.tr>
2017-07-21 00:12:49 +0300

    bahadir 20.07 son

---
+++ Bil372Proje/Bil372Proje/Pages/okul/ihtiyaclar/silme_ekrani.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Bil372Proje.Pages.okul.ihtiyaclar
+{
+    partial class silme_ekrani
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(218, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(707, 242);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ihtiyac_sil);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 63);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(656, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 63);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Geri";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // silme_ekrani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1135, 536);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "silme_ekrani";
+            this.Text = "silme_ekrani";
+            this.Load += new System.EventHandler(this.silme_ekrani_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Bil372Proje/Bil372Proje/Pages/okul/ihtiyaclar/silme_ekrani.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bil372Proje.Pages.okul.ihtiyaclar
+{
+    public partial class silme_ekrani : Form
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=bil372.database.windows.net;Initial Catalog=bil372DB;User ID=bahadir;Password=Qwerty123");
+        public string kAdi;
+        public int okul_id;
+        public silme_ekrani(string kullanici, int Id)
+        {
+            kAdi = kullanici;
+            okul_id = Id;
+            InitializeComponent();
+        }
+
+        private void silme_ekrani_Load(object sender, EventArgs e)
+        {
+            kayitGetir();
+        }
+        private void kayitGetir()
+        {
+
+            string kayit = "SELECT isim,marka,adet " +
+                          " from ihtiyac where ihtiyac.okul_id =@okul_id";
+
+            //musteriler tablosundaki tüm kayıtları çekecek olan sql sorgusu.
+            SqlCommand komut = new SqlCommand(kayit, con);
+            komut.Parameters.AddWithValue("@okul_id", okul_id);
+            //Sorgumuzu ve baglantimizi parametre olarak alan bir SqlCommand nesnesi oluşturuyoruz.
+            SqlDataAdapter da = new SqlDataAdapter(komut);
+            //SqlDataAdapter sınıfı verilerin databaseden aktarılması işlemini gerçekleştirir.
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            //Bir DataTable oluşturarak DataAdapter ile getirilen verileri tablo içerisine dolduruyoruz.
+            dataGridView1.DataSource = dt;
+            //Formumuzdaki DataGridViewin veri kaynağını oluşturduğumuz tablo olarak gösteriyoruz.
+            con.Close();
+        }
+
+
+
+        private void ihtiyac_sil(object sender, MouseEventArgs e)
+        {
+            sil();
+        }
+        private void sil()
+        {
+            con.Open();
+            string kullaniciAdi = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            SqlCommand cmd = new SqlCommand("select Id from okul where Id=@Id", con);
+            cmd.Parameters.AddWithValue("@kullanici_adi", kullaniciAdi);
+
+            con.Close();
+        }
+    }
+}
