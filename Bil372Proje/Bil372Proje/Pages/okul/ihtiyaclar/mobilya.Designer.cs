e59e2626c21835ef94dab0f9b5eccd4ebfcae68a
Daghan SENGOREN <daghan.sengoren@gmail.com>
2017-07-24 19:46:45 +0300

    bir kac tasarım iyilestirmesi

--- Bil372Proje/Bil372Proje/Pages/okul/ihtiyaclar/mobilya.Designer.cs
+++ Bil372Proje/Bil372Proje/Pages/okul/ihtiyaclar/mobilya.Designer.cs
@@ -50,9 +50,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(272, 70);
+            this.label1.Location = new System.Drawing.Point(204, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.Size = new System.Drawing.Size(53, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "İhtiyac: ";
             // 
@@ -60,9 +61,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(272, 158);
+            this.label2.Location = new System.Drawing.Point(204, 128);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.Size = new System.Drawing.Size(50, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Marka: ";
             // 
@@ -70,9 +72,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(272, 225);
+            this.label3.Location = new System.Drawing.Point(204, 183);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.Size = new System.Drawing.Size(37, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Adet:";
             // 
@@ -80,9 +83,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.label4.Location = new System.Drawing.Point(272, 297);
+            this.label4.Location = new System.Drawing.Point(204, 241);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 17);
+            this.label4.Size = new System.Drawing.Size(41, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Ölçü :";
             // 
@@ -90,37 +94,35 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(272, 364);
+            this.label5.Location = new System.Drawing.Point(204, 296);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(55, 17);
+            this.label5.Size = new System.Drawing.Size(45, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Renk: ";
             // 
             // mobilya_ihtiyac
             // 
             this.mobilya_ihtiyac.FormattingEnabled = true;
-            this.mobilya_ihtiyac.Location = new System.Drawing.Point(380, 66);
-            this.mobilya_ihtiyac.Margin = new System.Windows.Forms.Padding(4);
+            this.mobilya_ihtiyac.Location = new System.Drawing.Point(285, 54);
             this.mobilya_ihtiyac.Name = "mobilya_ihtiyac";
-            this.mobilya_ihtiyac.Size = new System.Drawing.Size(231, 24);
+            this.mobilya_ihtiyac.Size = new System.Drawing.Size(174, 21);
             this.mobilya_ihtiyac.TabIndex = 5;
             // 
             // mobilya_marka
             // 
             this.mobilya_marka.FormattingEnabled = true;
-            this.mobilya_marka.Location = new System.Drawing.Point(380, 150);
-            this.mobilya_marka.Margin = new System.Windows.Forms.Padding(4);
+            this.mobilya_marka.Location = new System.Drawing.Point(285, 122);
             this.mobilya_marka.Name = "mobilya_marka";
-            this.mobilya_marka.Size = new System.Drawing.Size(231, 24);
+            this.mobilya_marka.Size = new System.Drawing.Size(174, 21);
             this.mobilya_marka.TabIndex = 6;
             // 
             // mobilya_adet
             // 
             this.mobilya_adet.FormattingEnabled = true;
-            this.mobilya_adet.Location = new System.Drawing.Point(380, 225);
-            this.mobilya_adet.Margin = new System.Windows.Forms.Padding(4);
+            this.mobilya_adet.Location = new System.Drawing.Point(285, 183);
             this.mobilya_adet.Name = "mobilya_adet";
-            this.mobilya_adet.Size = new System.Drawing.Size(231, 24);
+            this.mobilya_adet.Size = new System.Drawing.Size(174, 21);
             this.mobilya_adet.TabIndex = 7;
             // 
             // mobilya_olcu
@@ -131,10 +133,9 @@
             "3 kisilik",
             "4 kisilik"});
             this.mobilya_olcu.FormattingEnabled = true;
-            this.mobilya_olcu.Location = new System.Drawing.Point(380, 297);
-            this.mobilya_olcu.Margin = new System.Windows.Forms.Padding(4);
+            this.mobilya_olcu.Location = new System.Drawing.Point(285, 241);
             this.mobilya_olcu.Name = "mobilya_olcu";
-            this.mobilya_olcu.Size = new System.Drawing.Size(231, 24);
+            this.mobilya_olcu.Size = new System.Drawing.Size(174, 21);
             this.mobilya_olcu.TabIndex = 8;
             // 
             // mobilya_renk
@@ -146,28 +147,27 @@
             "beyaz",
             "mavi"});
             this.mobilya_renk.FormattingEnabled = true;
-            this.mobilya_renk.Location = new System.Drawing.Point(380, 364);
-            this.mobilya_renk.Margin = new System.Windows.Forms.Padding(4);
+            this.mobilya_renk.Location = new System.Drawing.Point(285, 296);
             this.mobilya_renk.Name = "mobilya_renk";
-            this.mobilya_renk.Size = new System.Drawing.Size(231, 24);
+            this.mobilya_renk.Size = new System.Drawing.Size(174, 21);
             this.mobilya_renk.TabIndex = 9;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(645, 12);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Location = new System.Drawing.Point(484, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(461, 400);
+            this.dataGridView1.Size = new System.Drawing.Size(346, 325);
             this.dataGridView1.TabIndex = 10;
             // 
             // mobilya_ekle
             // 
-            this.mobilya_ekle.Location = new System.Drawing.Point(380, 442);
-            this.mobilya_ekle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.mobilya_ekle.Location = new System.Drawing.Point(285, 359);
+            this.mobilya_ekle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.mobilya_ekle.Name = "mobilya_ekle";
-            this.mobilya_ekle.Size = new System.Drawing.Size(109, 46);
+            this.mobilya_ekle.Size = new System.Drawing.Size(82, 37);
             this.mobilya_ekle.TabIndex = 11;
             this.mobilya_ekle.Text = "Ekle";
             this.mobilya_ekle.UseVisualStyleBackColor = false;
@@ -175,10 +175,10 @@
             // 
             // mobilya_geri
             // 
-            this.mobilya_geri.Location = new System.Drawing.Point(501, 442);
-            this.mobilya_geri.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.mobilya_geri.Location = new System.Drawing.Point(376, 359);
+            this.mobilya_geri.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.mobilya_geri.Name = "mobilya_geri";
-            this.mobilya_geri.Size = new System.Drawing.Size(109, 46);
+            this.mobilya_geri.Size = new System.Drawing.Size(82, 37);
             this.mobilya_geri.TabIndex = 12;
             this.mobilya_geri.Text = "Geri";
             this.mobilya_geri.UseVisualStyleBackColor = true;
@@ -188,9 +188,10 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label8.Location = new System.Drawing.Point(748, 476);
+            this.label8.Location = new System.Drawing.Point(561, 387);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(84, 25);
+            this.label8.Size = new System.Drawing.Size(67, 20);
             this.label8.TabIndex = 26;
             this.label8.Text = "Toplam";
             // 
@@ -198,18 +199,19 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label7.Location = new System.Drawing.Point(961, 469);
+            this.label7.Location = new System.Drawing.Point(722, 387);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(70, 25);
+            this.label7.Size = new System.Drawing.Size(57, 20);
             this.label7.TabIndex = 25;
             this.label7.Text = "label7";
             // 
             // mobilya
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(1119, 567);
+            this.ClientSize = new System.Drawing.Size(839, 461);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.mobilya_geri);
@@ -225,7 +227,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "mobilya";
             this.Text = "mobilya";
             this.Load += new System.EventHandler(this.mobilya_Load);
