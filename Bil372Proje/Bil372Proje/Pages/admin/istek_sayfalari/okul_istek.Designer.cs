7562aa5cf06505ed500ad73c45fb151b446ffe3d
bahadır çolak <bahdrcolak@outlook.com.tr>
2017-07-25 01:49:27 +0300

    tasarım

--- Bil372Proje/Bil372Proje/Pages/admin/istek_sayfalari/okul_istek.Designer.cs
+++ Bil372Proje/Bil372Proje/Pages/admin/istek_sayfalari/okul_istek.Designer.cs
@@ -37,6 +37,7 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.HighlightText;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
@@ -46,38 +47,43 @@
             // 
             // onay_click
             // 
+            this.onay_click.BackColor = System.Drawing.Color.LavenderBlush;
             this.onay_click.Location = new System.Drawing.Point(291, 444);
             this.onay_click.Name = "onay_click";
             this.onay_click.Size = new System.Drawing.Size(162, 63);
             this.onay_click.TabIndex = 1;
             this.onay_click.Text = "Onayla";
-            this.onay_click.UseVisualStyleBackColor = true;
+            this.onay_click.UseVisualStyleBackColor = false;
             this.onay_click.Click += new System.EventHandler(this.onay_click_Click);
             // 
             // geri_btn
             // 
+            this.geri_btn.BackColor = System.Drawing.Color.LavenderBlush;
             this.geri_btn.Location = new System.Drawing.Point(780, 444);
             this.geri_btn.Name = "geri_btn";
             this.geri_btn.Size = new System.Drawing.Size(162, 63);
             this.geri_btn.TabIndex = 2;
             this.geri_btn.Text = "Geri";
-            this.geri_btn.UseVisualStyleBackColor = true;
+            this.geri_btn.UseVisualStyleBackColor = false;
             this.geri_btn.Click += new System.EventHandler(this.geri_btn_Click);
             // 
             // sil_btn
             // 
+            this.sil_btn.BackColor = System.Drawing.Color.LavenderBlush;
             this.sil_btn.Location = new System.Drawing.Point(527, 444);
             this.sil_btn.Name = "sil_btn";
             this.sil_btn.Size = new System.Drawing.Size(162, 63);
             this.sil_btn.TabIndex = 3;
             this.sil_btn.Text = "Sil";
-            this.sil_btn.UseVisualStyleBackColor = true;
+            this.sil_btn.UseVisualStyleBackColor = false;
             this.sil_btn.Click += new System.EventHandler(this.sil_btn_Click);
             // 
             // okul_istek
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.BackgroundImage = global::Bil372Proje.Properties.Resources.homeBox_fdcd3087cd12079a35c76302932e6356;
             this.ClientSize = new System.Drawing.Size(1172, 590);
             this.Controls.Add(this.sil_btn);
             this.Controls.Add(this.geri_btn);
--- Bil372Proje/Bil372Proje/Pages/admin/silme_sayfalari/tedarikci_sil.Designer.cs
+++ Bil372Proje/Bil372Proje/Pages/admin/silme_sayfalari/tedarikci_sil.Designer.cs
@@ -36,40 +36,49 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(156, 261);
+            this.button1.BackColor = System.Drawing.Color.LavenderBlush;
+            this.button1.Location = new System.Drawing.Point(169, 390);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 42);
+            this.button1.Size = new System.Drawing.Size(152, 52);
             this.button1.TabIndex = 0;
             this.button1.Text = "Tedarikçiyi Sil";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(429, 261);
+            this.button2.BackColor = System.Drawing.Color.LavenderBlush;
+            this.button2.Location = new System.Drawing.Point(548, 390);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 41);
+            this.button2.Size = new System.Drawing.Size(149, 50);
             this.button2.TabIndex = 1;
             this.button2.Text = "Geri";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.HighlightText;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(64, 37);
+            this.dataGridView1.Location = new System.Drawing.Point(59, 13);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(590, 180);
+            this.dataGridView1.Size = new System.Drawing.Size(791, 322);
             this.dataGridView1.TabIndex = 2;
             // 
             // tedarikci_sil
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(741, 349);
+            this.BackColor = System.Drawing.Color.Azure;
+            this.BackgroundImage = global::Bil372Proje.Properties.Resources.homeBox_fdcd3087cd12079a35c76302932e6356;
+            this.ClientSize = new System.Drawing.Size(898, 496);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "tedarikci_sil";
             this.Text = "tedarikci_sil";
             this.Load += new System.EventHandler(this.tedarikci_sil_Load);
