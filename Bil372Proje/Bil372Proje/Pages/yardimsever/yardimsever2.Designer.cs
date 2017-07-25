058d31a3ba6aa5340807771f13f3f7c592d59de4
Mberk <Mberk@DESKTOP-0A77KSN>
2017-07-25 03:24:33 +0300

    Tasarımlar tamamlandı.

--- Bil372Proje/Bil372Proje/Pages/yardimsever/yardimsever2.Designer.cs
+++ Bil372Proje/Bil372Proje/Pages/yardimsever/yardimsever2.Designer.cs
@@ -38,65 +38,73 @@
             // 
             // dataGridView1
             // 
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.Azure;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(579, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(434, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(540, 551);
+            this.dataGridView1.Size = new System.Drawing.Size(405, 448);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.goto_okulpage);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(378, 345);
+            this.button3.BackColor = System.Drawing.Color.LavenderBlush;
+            this.button3.Location = new System.Drawing.Point(284, 280);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(118, 50);
+            this.button3.Size = new System.Drawing.Size(88, 41);
             this.button3.TabIndex = 3;
             this.button3.Text = "Geri";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // okul_adi
             // 
-            this.okul_adi.Location = new System.Drawing.Point(264, 247);
+            this.okul_adi.Location = new System.Drawing.Point(198, 201);
+            this.okul_adi.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.okul_adi.Name = "okul_adi";
-            this.okul_adi.Size = new System.Drawing.Size(232, 22);
+            this.okul_adi.Size = new System.Drawing.Size(175, 20);
             this.okul_adi.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(71, 247);
+            this.label1.Location = new System.Drawing.Point(53, 201);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.Size = new System.Drawing.Size(63, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Okul Adı :";
             // 
             // okul_ara
             // 
-            this.okul_ara.Location = new System.Drawing.Point(165, 346);
+            this.okul_ara.BackColor = System.Drawing.Color.LavenderBlush;
+            this.okul_ara.Location = new System.Drawing.Point(124, 281);
+            this.okul_ara.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.okul_ara.Name = "okul_ara";
-            this.okul_ara.Size = new System.Drawing.Size(118, 49);
+            this.okul_ara.Size = new System.Drawing.Size(88, 40);
             this.okul_ara.TabIndex = 6;
             this.okul_ara.Text = "Ara";
-            this.okul_ara.UseVisualStyleBackColor = true;
+            this.okul_ara.UseVisualStyleBackColor = false;
             this.okul_ara.Click += new System.EventHandler(this.okul_ara_Click);
             // 
             // yardimsever2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Azure;
-            this.ClientSize = new System.Drawing.Size(1131, 575);
+            this.BackgroundImage = global::Bil372Proje.Properties.Resources.homeBox_fdcd3087cd12079a35c76302932e6356;
+            this.ClientSize = new System.Drawing.Size(848, 467);
             this.Controls.Add(this.okul_ara);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.okul_adi);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "yardimsever2";
-            this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
